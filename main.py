b5da8280d6d1248a61182395a5807cc7044a5dce
Peter Yang <me@peteryang.io>
2019-02-02 01:15:47 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    print("let's do gyroscope stuff lululul")
+    time.sleep(3)
+    print("done let's upload this stuff to google hahaha")
+    return "Hello world!"
+
+
+if __name__ == "__main__":
+    app.run()
