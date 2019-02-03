cbdcd7cb44dbde10a25c598def3863012217a07a
Sean Park <sean_park@college.harvard.edu>
2019-02-02 21:33:05 -0800

    dragon update

--- firebase.py
+++ firebase.py
@@ -1,13 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import datetime
 
 
 class Collection:
     def __init__(self):
-        cred = credentials.Certificate('service.json')
+        cred = credentials.Certificate('dragon.json')
         firebase_admin.initialize_app(cred)
         self.db = firestore.client()
+
 
     def dataup(self):
         doc_ref = self.db.collection(u'users').document(u'alovelace')
@@ -15,4 +17,18 @@ class Collection:
             u'first': u'Ada',
             u'last': u'Lovelace',
             u'born': 1815
+        })
+
+    def readit(self):
+        commands_ref = self.db.collection(u'commands')
+        docs = commands_ref.get()
+
+        for doc in docs:
+            print(u'{} => {}'.format(doc.id, doc.to_dict()))
+
+    def create_sub(self, command_id, gyro_data):
+        command_a_ref = self.db.collection(u'commands').document(command_id)
+        command_a_ref.collection(u'attempts').add({
+            u'gyroData': gyro_data,
+            u'timestamp': datetime.datetime.now().isoformat()
         })
--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from firebase import Collection
 
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 collection = Collection()
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST']))
 def index():
     print("let's do gyroscope stuff lululul")
     print("done let's upload this stuff to google hahaha")
-    collection.dataup()
+    if request.method == 'POST':
+        # begin recording
+        'command_id' = request.values.get('command_id')
+    else:
+        # end recording and upload attempt to subcollection for command
+        # gyro_data = some_recording
+        # collection.create_sub(command_id, gyro_data)
+        collection.dataup()
     return "Hello world!"
-
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
